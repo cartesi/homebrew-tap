10ffc95a8bdc87da3c6f4524d728deb5e7c9a7ef
Danilo Tuler <tuler@pobox.com>
2024-05-08 11:22:10 -0400

    feat: bump version to 0.14.1

--- Formula/cartesi.rb
+++ Formula/cartesi.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Cartesi < Formula
   desc "Cartesi CLI"
   homepage "https://github.com/cartesi/cli"
-  url "https://registry.npmjs.org/@cartesi/cli/-/cli-0.14.0.tgz"
-  sha256 "81333a40e9a982cac90d219127f2faaa7a3e44f5cbb910aef96dcc313a89e56f"
+  url "https://registry.npmjs.org/@cartesi/cli/-/cli-0.14.1.tgz"
+  sha256 "dafff6a50d493c0acbe192e636a290628d52fbc2452f11f94b21c540533abb7a"
   license "Apache-2.0"
 
   livecheck do
