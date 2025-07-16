2473e1553021972d167e84b8b6b55f31da8e414a
Alex Mikhalevich <2990126+alexmikhalevich@users.noreply.github.com>
2025-07-16 18:47:32 +0000

    cartesi-machine-rootfs-image: add 0.20.0 bottle.

--- Formula/cartesi-machine-rootfs-image.rb
+++ Formula/cartesi-machine-rootfs-image.rb
@@ -1,6 +1,13 @@
 class CartesiMachineRootfsImage < Formula
   desc "Rootfs image for the Cartesi Machine"
   homepage "https://github.com/cartesi/machine-guest-tools"
+
+  bottle do
+    root_url "https://ghcr.io/v2/cartesi/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "8143010ab08b493b315c4cb9d2c048f410dfb58ec77c42e9a3f93b5ed5805585"
+    sha256                               ventura:      "9fa0384f4a16e1269f8103b63f74407572352f732b00b2d01398b99ab40dc82b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "6372d211183ce4852a94c8a537651e2ad8fcb2e51f9aa75f7bf081ee54b2e706"
+  end
   VERSION = "v0.20.0-test1".freeze
   ROOTFS_IMAGE = "rootfs-ubuntu.ext2".freeze
   url "https://github.com/cartesi/machine-rootfs-image/releases/download/#{VERSION}/#{ROOTFS_IMAGE}"
