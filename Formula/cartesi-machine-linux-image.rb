4c3ba125bfa30f4a094878151eb1d841de5d189c
Danilo Tuler <tuler@pobox.com>
2026-04-10 15:59:42 -0400

    fix: new auditing rule
    Closes #7.
    
    Signed-off-by: Danilo Tuler <73850+tuler@users.noreply.github.com>

--- Formula/cartesi-machine-linux-image.rb
+++ Formula/cartesi-machine-linux-image.rb
@@ -1,6 +1,11 @@
 class CartesiMachineLinuxImage < Formula
   desc "Kernel image for the Cartesi Machine"
   homepage "https://github.com/cartesi/machine-linux-image"
+  VERSION = "0.20.0".freeze
+  LINUX_IMAGE = "linux-6.5.13-ctsi-1-v#{VERSION}.bin".freeze
+  url "https://github.com/cartesi/image-kernel/releases/download/v#{VERSION}/#{LINUX_IMAGE}"
+  sha256 "65dd100ff6204346ac2f50f772721358b5c1451450ceb39a154542ee27b4c947"
+  license "GPL-2.0-only"
 
   bottle do
     root_url "https://ghcr.io/v2/cartesi/tap"
@@ -8,11 +13,6 @@ class CartesiMachineLinuxImage < Formula
     sha256 cellar: :any_skip_relocation, ventura:      "096a8524db3dc659c957a0186cf6b34d95eff25e48325652e96cbad843406727"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "9a025f231e8f5a76bcdaca2ed1e284e1ac8f7996f219b7c6fbcc2d92206fcdee"
   end
-  VERSION = "0.20.0".freeze
-  LINUX_IMAGE = "linux-6.5.13-ctsi-1-v#{VERSION}.bin".freeze
-  url "https://github.com/cartesi/image-kernel/releases/download/v#{VERSION}/#{LINUX_IMAGE}"
-  sha256 "65dd100ff6204346ac2f50f772721358b5c1451450ceb39a154542ee27b4c947"
-  license "GPL-2.0-only"
 
   def install
     share.install self.class::LINUX_IMAGE
