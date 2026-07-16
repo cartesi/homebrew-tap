d51fd0ea3c5a25c18cc0cac51da1a42aaf90f573
Danilo Tuler <tuler@pobox.com>
2026-07-16 11:30:50 -0400

    feat: cartesi-machine 0.21.0
    Closes #12.
    
    Signed-off-by: Danilo Tuler <73850+tuler@users.noreply.github.com>

--- Formula/cartesi-machine.rb
+++ Formula/cartesi-machine.rb
@@ -1,16 +1,9 @@
 class CartesiMachine < Formula
   desc "Meta package for the Cartesi Machine"
   homepage "https://cartesi.io"
-  url "https://github.com/cartesi/machine-emulator/archive/refs/tags/v0.19.0.tar.gz"
-  sha256 "1a33fb7e0dd7030656ddbb214dcd2f5a569f64e906edb20f149b66b34e1e0dd3"
+  url "https://github.com/cartesi/machine-emulator/archive/refs/tags/v0.21.0.tar.gz"
+  sha256 "44fc83cb08a1907e66ab225ccf421b0fdad3f145be35b9a9ffe561d3563e3eda"
   license "LGPL-3.0-only"
-
-  bottle do
-    root_url "https://ghcr.io/v2/cartesi/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "8edb6724dffd2ad487d61da3eaab1781f2eb96a65a71af696c812e6286b39bfa"
-    sha256 cellar: :any_skip_relocation, ventura:      "d2c7400e88c12432cf55971ea9ae6edbaf72fa30c14ceeafd0b3f17148b6ffcb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "b3da1acd61897dfd758e257c433140d5e8f53260beec15cb1b21933f90d41e48"
-  end
 
   depends_on "cartesi-machine-emulator"
   depends_on "cartesi-machine-linux-image"
