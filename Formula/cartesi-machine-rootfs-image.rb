a401bd0996ab86b7c88fbcdd222d3e3eecbc9c9f
Danilo Tuler <tuler@pobox.com>
2026-08-03 17:10:35 -0400

    feat: cartesi-machine-rootfs-image 0.21.0

--- Formula/cartesi-machine-rootfs-image.rb
+++ Formula/cartesi-machine-rootfs-image.rb
@@ -1,17 +1,12 @@
 class CartesiMachineRootfsImage < Formula
   desc "Rootfs image for the Cartesi Machine"
   homepage "https://github.com/cartesi/machine-guest-tools"
-  VERSION = "v0.20.0-test1".freeze
-  ROOTFS_IMAGE = "rootfs-ubuntu.ext2".freeze
-  url "https://github.com/cartesi/machine-rootfs-image/releases/download/#{VERSION}/#{ROOTFS_IMAGE}"
-  sha256 "a38dbf39bdadc644bb31f1fc427be037b3082c9007afe1d9f2220b20d2789271"
+  VERSION = "v0.18.0".freeze
+  ROOTFS_IMAGE = "rootfs-tools.ext2".freeze
+  url "https://github.com/cartesi/machine-guest-tools/releases/download/#{VERSION}/#{ROOTFS_IMAGE}"
+  version "0.21.0"
+  sha256 "6c159937485c99f695021c4f2ea2a57bdadcf4e4bce8e71af5bee3bb9552802e"
   license "GPL-2.0-only"
-
-  bottle do
-    root_url "https://ghcr.io/v2/cartesi/tap"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "aee58d733584f6f45609a4fb1055a51baf36542fbfd4e51e0e6327056e333cab"
-  end
 
   def install
     share.install self.class::ROOTFS_IMAGE
