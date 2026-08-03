b11e9423d995e20adc14a21bde2c3169f04bb0d7
Danilo Tuler <tuler@pobox.com>
2026-08-03 14:16:41 -0400

    feat: cartesi-machine-linux-image 0.21.0

--- Formula/cartesi-machine-linux-image.rb
+++ Formula/cartesi-machine-linux-image.rb
@@ -1,17 +1,11 @@
 class CartesiMachineLinuxImage < Formula
   desc "Kernel image for the Cartesi Machine"
   homepage "https://github.com/cartesi/machine-linux-image"
-  VERSION = "0.20.0".freeze
-  LINUX_IMAGE = "linux-6.5.13-ctsi-1-v#{VERSION}.bin".freeze
-  url "https://github.com/cartesi/image-kernel/releases/download/v#{VERSION}/#{LINUX_IMAGE}"
-  sha256 "65dd100ff6204346ac2f50f772721358b5c1451450ceb39a154542ee27b4c947"
-  license "GPL-2.0-only"
-
-  bottle do
-    root_url "https://ghcr.io/v2/cartesi/tap"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "ac3a16d05cdc90a34f7c4dd868cd91163c0ea0be5045d35ccb9cbe4318b8cca3"
-  end
+  VERSION = "0.21.0".freeze
+  LINUX_IMAGE = "linux-6.5.13-ctsi-2-v#{VERSION}.bin".freeze
+  url "https://github.com/cartesi/machine-linux-image/releases/download/v#{VERSION}/#{LINUX_IMAGE}"
+  sha256 "5c900060da2db2bfa84cd39cd9cd722988c83c42225f3cac55f2d3157e48f32f"
+  license "Apache-2.0"
 
   def install
     share.install self.class::LINUX_IMAGE
