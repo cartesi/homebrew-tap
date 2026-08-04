29f79dee81665750ec0f4744be0bf5d71e0a9c93
Danilo Tuler <73850+tuler@users.noreply.github.com>
2026-08-04 20:13:01 +0000

    cartesi-machine-linux-image: add 0.21.0 bottle.

--- Formula/cartesi-machine-linux-image.rb
+++ Formula/cartesi-machine-linux-image.rb
@@ -1,6 +1,11 @@
 class CartesiMachineLinuxImage < Formula
   desc "Kernel image for the Cartesi Machine"
   homepage "https://github.com/cartesi/machine-linux-image"
+
+  bottle do
+    root_url "https://ghcr.io/v2/cartesi/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "d5f151c66fe5841e77c41e8e213728f294cc91911867c238ecf31ed1101aef7b"
+  end
   VERSION = "0.21.0".freeze
   LINUX_IMAGE = "linux-6.5.13-ctsi-2-v#{VERSION}.bin".freeze
   url "https://github.com/cartesi/machine-linux-image/releases/download/v#{VERSION}/#{LINUX_IMAGE}"
