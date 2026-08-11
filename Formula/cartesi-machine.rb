4388b9903c53acb6e5b274686e7d80b17ebd8b70
Danilo Tuler <73850+tuler@users.noreply.github.com>
2026-08-11 15:15:47 +0000

    cartesi-machine: add 0.21.0_1 bottle.

--- Formula/cartesi-machine.rb
+++ Formula/cartesi-machine.rb
@@ -6,6 +6,12 @@ class CartesiMachine < Formula
   license "LGPL-3.0-only"
   revision 1
 
+  bottle do
+    root_url "https://ghcr.io/v2/cartesi/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "73ee8d279c620a9d12f4408434d67d36b4321277ca50a55091dfb6dfd9d3998e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "78deda26b74a3a52a13e7dbf210b0e1f2d85b0835227660dd5531368dd1f6bbb"
+  end
+
   depends_on "cartesi-machine-emulator"
   depends_on "cartesi-machine-linux-image"
   depends_on "cartesi-machine-rootfs-image"
